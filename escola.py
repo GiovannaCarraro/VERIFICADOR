ccba74cbd6e0a14511f335629db71e286d848e00
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-03-06 17:27:21 -0300

    add: terminando

--- escola.py
+++ escola.py
@@ -1,12 +1,24 @@
-def verificador_media(media:int|float) -> str:
+def verificador_media(media:int|float) -> str :
    """"Está função retorna se o aluno passou ou não de ano"""
 
+   #Testando para ver se a média é um número
+   if not isinstance(media, (int, float)):
+      raise TypeError("Tipo inválido, a entrada deve ser númerica")
+
+   if media < 0:
+      raise ValueError("O valor deve ser menor ou igual a 0 ou igual a 10")
+   
+   if media > 10:
+      raise ValueError("O valor deve ser maior ou igual a 0 ou igual a 10")
+   
    if media >= 7:
       return "Aprovado"
-   elif media < 5:
+   elif media <= 4:
       return "Reprovado"
    else:
       return "Recuperação"
+   
+
    
 #if __name__ == "__main__":
    #print(verificador_media(6))
--- test/test_verificador_media_erros.py
+++ test/test_verificador_media_erros.py
@@ -4,3 +4,11 @@ from escola import verificador_media
 def test_string_entrada():
     with pytest.raises(TypeError, match="Tipo inválido, a entrada deve ser númerica"):
         verificador_media("CASA")
+
+def test_numero_negativo():
+    with pytest.raises(ValueError, match="O valor deve ser menor ou igual a 0 ou igual a 10"):
+        verificador_media(-5)
+
+def test_numero_maior():
+    with pytest.raises(ValueError, match="O valor deve ser maior ou igual a 0 ou igual a 10"):
+        verificador_media(2000)
